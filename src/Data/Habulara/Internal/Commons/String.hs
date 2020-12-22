8e6e3d65325f6154d2f19cacc8d64a0e927ca89f
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2020-12-22 14:32:06 +0800

    feat: add code generator for Haskell `data` definitions

--- src/Data/Habulara/Internal/Commons/String.hs
+++ src/Data/Habulara/Internal/Commons/String.hs
@@ -1,6 +1,6 @@
 module Data.Habulara.Internal.Commons.String where
 
-import Data.Char (toLower)
+import Data.Char (toLower, toUpper)
 
 
 -- | Lowers the first letter of the given 'String'.
@@ -12,3 +12,14 @@ import Data.Char (toLower)
 lowerFirst :: String -> String
 lowerFirst []       = []
 lowerFirst (x : xs) = toLower x : xs
+
+
+-- | Uppers the first letter of the given 'String'.
+--
+-- >>> upperFirst ""
+-- ""
+-- >>> upperFirst "aaa"
+-- "Aaa"
+upperFirst :: String -> String
+upperFirst []       = []
+upperFirst (x : xs) = toUpper x : xs
