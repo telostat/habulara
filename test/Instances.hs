18cbbc07a73081f095a782d2915a26c2b82dc3ac
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2021-01-29 09:30:44 +0800

    refactor: start with tests (QuickCheck), ditch Examples, reorganize
    We will use QuickCheck in our tests. Added a few laws and instances to
    do some property checking. We will most likely depend on Hspec soon, as
    well. More to come...
    
    We added a new directory under the repository: `var/dev/`. All
    development related stuff (such as examples, benchmark scripts, data
    generators, etc.) goes into this directory.

---
+++ test/Instances.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Instances where
+
+import qualified Data.ByteString           as B
+import qualified Data.ByteString.Char8     as BC
+import           Data.Char                 (chr)
+import           Data.Habulara.Core        (NonEmpty(..), Value(..))
+import qualified Data.Text                 as T
+import           Test.QuickCheck           (Arbitrary(..), choose, listOf1)
+import           Test.QuickCheck.Instances ()
+
+
+---------------
+-- NON-EMPTY --
+---------------
+
+instance Arbitrary (NonEmpty B.ByteString) where
+  -- TODO: Make sure that we produce Unicode characters, too.
+  arbitrary = MkNonEmpty . BC.pack <$> listOf1 (chr <$> choose (32, 126))
+
+
+instance Arbitrary (NonEmpty T.Text) where
+  -- TODO: Make sure that we produce Unicode characters, too.
+  arbitrary = MkNonEmpty . T.pack <$> listOf1 (chr <$> choose (32, 126))
+
+
+-------------------------------
+-- ARBITRARY INSTANCE: VALUE --
+-------------------------------
+
+
+instance Arbitrary Value where
+  arbitrary = do
+    n <- choose (1, 8 :: Int)
+    case n of
+      1 -> pure VEmpty
+      2 -> VRaw      <$> arbitrary
+      3 -> VText     <$> arbitrary
+      4 -> VInt      <$> arbitrary
+      5 -> VDecimal  <$> arbitrary
+      6 -> VBoolean  <$> arbitrary
+      7 -> VDate     <$> arbitrary
+      8 -> VDateTime <$> arbitrary
