471c1c12a8d2032b855c499634f344550409162d
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2021-02-01 11:38:50 +0800

    refactor: purge old implementation
    We will focus first on splitting Habulara into sub-packages and then
    on DSL implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,34 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import qualified Data.ByteString                    as B
-import qualified Data.ByteString.Lazy               as BL
-import qualified Data.ByteString.Lazy.Char8         as BLC
-import           Data.Habulara.Dsl                  (processWithHab, readHab)
-import           Data.Habulara.Internal.Dsl.CodeGen (generateData)
-import qualified Data.Text                          as T
-import           System.Environment                 (getArgs)
-import           System.Exit                        (die, exitFailure)
-import           System.IO                          (hPutStrLn, stderr, stdout)
+import qualified Data.ByteString.Lazy         as BL
+import           Data.Habulara.Core.Conduit   (runMapperIntoHandle)
+import           Data.Habulara.Core.Operation
+import           System.Environment           (getArgs)
+import           System.IO                    (hPutStrLn, stderr, stdout)
 
 
 main :: IO ()
 main = do
-  eprog <- getProgramArguments =<< getArgs
-  case eprog of
-    Left habpath -> do
-      hab <- B.readFile habpath
-      case readHab hab of
-        Left err -> hPutStrLn stderr ("Can not read HAB file: " <> err) >> exitFailure
-        Right fs -> putStrLn $ T.unpack $ generateData fs
-    Right (habpath, csvpath) -> do
-      hab <- B.readFile habpath
-      csv <- BL.readFile csvpath
-      case processWithHab hab csv of
-        Left err -> hPutStrLn stderr ("Error while processing the file: " <> err) >> exitFailure
-        Right oc -> BLC.hPutStr stdout oc
-
-
-getProgramArguments :: [FilePath] -> IO (Either FilePath (FilePath, FilePath))
-getProgramArguments [habfile]          = pure $ Left habfile
-getProgramArguments [habfile, csvfile] = pure $ Right (habfile, csvfile)
-getProgramArguments _                  = die "Incorrect number of arguments. Exiting..."
+  [filepath] <- getArgs
+  content <- BL.readFile filepath
+  x <- runMapperIntoHandle ',' fops True content stdout
+  either print (hPutStrLn stderr . ("Total number of records processed: " <>) . show . snd) x
+  where
+    fops =
+      [ ("id", select "id")
+      , ("name", select "name")
+      , ("tempCelcius", select "temperature")
+      , ("tempFahrenheit", select "temperature" >>= asNumber >>= multiply (number 9) >>= flip divide (number 5) >>= add (number 32))
+      , ("precipitation", select "precipitation" >>= asNumber >>= percentage)
+      ]
--- src/Data/Habulara/Dsl.hs
+++
@@ -1,7 +0,0 @@
-module Data.Habulara.Dsl
-  ( module Types
-  , module Utils
-  ) where
-
-import Data.Habulara.Internal.Dsl.Types as Types
-import Data.Habulara.Internal.Dsl.Utils as Utils
