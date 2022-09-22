df031b7651462580101b20cbe706c83e417ac4cc
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2022-09-22 08:31:12 +0800

    chore(build): enable static builds using Nix

--- default.nix
+++ default.nix
@@ -13,7 +13,26 @@ let
   ## Get the haskell set:
   haskell = pkgs.haskell.packages.${compiler};
 
+  ## Define a function that makes this application statically compiled:
+  makeAppStatic = drv: pkgs.haskell.lib.compose.overrideCabal
+    (_: {
+      enableSharedExecutables = false;
+      enableSharedLibraries = false;
+      doCheck = false;
+      configureFlags = [
+        "--ghc-option=-optl=-static"
+        "--ghc-option=-optl=-pthread"
+        # "--ghc-option=-fPIC"
+        "--extra-lib-dirs=${pkgs.glibc.static}/lib"
+        "--extra-lib-dirs=${pkgs.gmp6.override { withStatic = true; }}/lib"
+        "--extra-lib-dirs=${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib"
+      ];
+    })
+    drv;
+
   ## Get this package:
   thisPackage = haskell.callCabal2nixWithOptions "habulara" ./. "--no-check --no-haddock" { };
 in
-pkgs.haskell.lib.justStaticExecutables thisPackage
+if doStatic
+then makeAppStatic thisPackage
+else pkgs.haskell.lib.justStaticExecutables thisPackage
